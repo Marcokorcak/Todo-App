fd9a9474e5ca5f83ffe1c53bf4ccdbdd85ac1429
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-05-15 23:31:55 -0400

    started login page

---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+import Login from "@/components/Login";
+
+const PageLog = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default PageLog;
---
+++ components/Login.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { useState } from "react";
+const Login = () => {
+  return (
+    <div>
+      <p>Login page</p>
+    </div>
+  );
+};
+
+export default Login;
