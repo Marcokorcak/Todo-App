f4e75fbc9c48cde462a16e38ec688eede4980260
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-05-16 18:27:30 -0400

    did register styling

---
+++ app/register/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Register from "@/components/Register";
+
+const PageReg = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default PageReg;
