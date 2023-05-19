6239a53980d39d32dcde7dc7c4a240001f6fa8ca
Marco Korcak <marcokorcak@Marcos-Air.fios-router.home>
2023-05-18 23:43:49 -0400

    created the login page with checks and verification

--- components/Login.jsx
+++ components/Login.jsx
@@ -3,67 +3,109 @@
 import React, { useState } from "react";
 import { loginUser } from "@/app/utils/data";
 import { useReducer } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import useUser from "@/hooks/useUser";
 
 const Login = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const { user } = useUser();
+  const router = useRouter();
+
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "email":
+      case "password":
+        return { ...state, [action.type]: action.value };
+      case "loading":
+        return { ...state, loading: action.value };
+      case "response":
+        return { ...state, response: action.value };
+    }
+
+    throw Error("Unknown action." + action.type);
+  }
+
+  const initialState = {
+    email: "",
+    password: "",
+    response: "",
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { email, password, response, loading } = state;
+
+  const login = async (e) => {
+    dispatch({ type: "loading", value: true });
+    dispatch({ type: "response", value: null });
+    e.preventDefault();
+
+    const response = await loginUser(email, password);
+
+    dispatch({ type: "response", value: response });
+    dispatch({ type: "loading", value: false });
+    if (!!response?.success) {
+      setTimeout(() => {
+        router.replace("/profile");
+      }, 3000);
+    }
+  };
 
   return (
     <div className="card flex flex-col items-center bg-violet-300 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl dark:border-violet-300 dark:bg-violet-300 dark:hover:bg-violet-300">
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h1>LOGIN</h1>
-        <div className="form__group field">
-          <input
-            class="form__field"
-            placeholder="User Name"
-            name="username"
-            id="username"
-            required
-            type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-          <input
-            className="form__field"
-            placeholder="Password"
-            name="password"
-            id="pass"
-            required
-            type="text"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <br />
-        <div className="btn">
-          <a
-            href="#_"
-            className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-violet-600 transition duration-300 ease-out border-2 border-violet-600 rounded-full shadow-md group"
+      <div className="flex flex-col justify-between p-2 leading-normal">
+        {response && (
+          <div
+            className={`${
+              response.success
+                ? "bg-green-200 border-2 border-green-800 text-green-800"
+                : "bg-red-200 border-2 border-red-800 text-red-800"
+            } py-2 px-5 my-10 text-center`}
           >
-            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-violet-600 group-hover:translate-x-0 ease">
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M14 5l7 7m0 0l-7 7m7-7H3"
-                ></path>
-              </svg>
+            <span className="font-bold">
+              {response.success
+                ? `Success ${response.message}`
+                : `Failure: ${response.error.message}`}
             </span>
-            <span className="absolute flex items-center justify-center w-full h-full text-violet-600 transition-all duration-300 transform group-hover:translate-x-full ease">
-              Submit
-            </span>
-            <span className="relative invisible">Submit</span>
-          </a>
-        </div>
+          </div>
+        )}
+        <h1 className="my-8 h1 text-center">Login to your Account</h1>
+        <form
+          onSubmit={login}
+          className={loading ? "opacity-[10%] pointer-events-none" : ""}
+        >
+          {/* ["email", "name", "slug", "password", "response", "loading"] */}
+          {Object.keys(initialState)
+            .filter((k) => !["response", "loading"].includes(k))
+            .map((key) => {
+              let type = "text";
+              if (key === "password") {
+                type = "password";
+              } else if (key === "email") {
+                type = "email";
+              }
+
+              return (
+                <p key={key} className="mb-2">
+                  <label className="h3 capitalize w-[75px] inline-block"></label>
+                  <input
+                    className="form__field"
+                    placeholder={key}
+                    required
+                    name={key}
+                    onChange={(e) => {
+                      dispatch({ type: e.target.name, value: e.target.value });
+                    }}
+                    value={state[key]}
+                    type={type}
+                  />
+                </p>
+              );
+            })}
+          <div className="flex justify-center my-10">
+            <input className="button small" type="submit"></input>
+          </div>
+        </form>
       </div>
     </div>
   );
