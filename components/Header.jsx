c9288034e9ab3dd3b627268b0ebd4b4e6b546506
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-05-22 18:10:02 -0400

    made background responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -27,29 +27,33 @@ const Header = () => {
 
   if (user) {
     return (
-      <nav className="header">
+      <nav className="header flex gap-10">
         <p>
           <FontAwesomeIcon icon={faUser} /> {name}
         </p>
-        <NavButton link="/" name="Home" />
-        <NavButton link={`/user/${user.id}`} name="My Lists" />
+        <NavButton link="/" name="Home" className="lstyles" />
+        <NavButton
+          link={`/user/${user.id}`}
+          name="My Lists"
+          className="lstyles"
+        />
         {/*  <NavButton link="/contact" name="User List" /> */}
         {/* <NavButton link="/register" name="Register" /> */}
-        <NavButton link="/listnew" name="Create" />
-        <NavButton link="/logout" name="Logout" />
+        <NavButton link="/listnew" name="Create" className="lstyles" />
+        <NavButton link="/logout" name="Logout" className="lstyles" />
       </nav>
     );
   } else if (!user) {
     return (
-      <nav className="header">
+      <nav className="header flex gap-10">
         <p>
           <FontAwesomeIcon icon={faUser} /> {name}
         </p>
-        <NavButton link="/" name="Home" />
+        <NavButton link="/" name="Home" className="lstyles" />
         {/* <NavButton link="/login" name="My Account" /> */}
-        <NavButton link="/login" name="User List" />
-        <NavButton link="/login" name="Login" />
-        <NavButton link="/register" name="Register" />
+        <NavButton link="/login" name="User List" className="lstyles" />
+        <NavButton link="/login" name="Login" className="lstyles" />
+        <NavButton link="/register" name="Register" className="lstyles" />
       </nav>
     );
   }
