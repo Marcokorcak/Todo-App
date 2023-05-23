7a06558ab8dc09410a0850dd91422a58fd32f015
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-05-23 00:29:06 -0400

    removed unecessary files

--- app/user/[user_id]/list/[id]/page.js
+++ app/user/[user_id]/list/[id]/page.js
@@ -20,8 +20,6 @@ const Page = ({ params: { id } }) => {
 
   return (
     <section className={"mx-auto max-w-7xl px-4 py-5 "}>
-      <div className="w-full max-w-screen-xl mx-auto px-6 ">
-        <div className="flex justify-center p-4 px-3 py-10 ">
           <div className="w-full max-w-md">
             <div className="bg-lime-200 shadow-md border-2 border-black rounded-lg px-3 py-2 mb-4">
               <div className="block text-gray-700 text-2xl font-semibold py-2 px-2">
@@ -72,8 +70,6 @@ const Page = ({ params: { id } }) => {
               </div>
             </div>
           </div>
-        </div>
-      </div>
     </section>
   );
 };
