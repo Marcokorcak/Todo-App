41133dcb280deb56c1921b6a597da34bd12999c4
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-05-22 20:36:13 -0400

    minor fix

--- app/user/[user_id]/list/[id]/page.js
+++ app/user/[user_id]/list/[id]/page.js
@@ -22,7 +22,7 @@ const Page = ({ params: { id } }) => {
     <section className={"mx-auto max-w-7xl px-4 py-5 "}>
       <div className="w-full max-w-screen-xl mx-auto px-6 ">
         <div className="flex justify-center p-4 px-3 py-10 ">
-          <div className="w-full max-w-md bg-lime-200">
+          <div className="w-full max-w-md">
             <div className="bg-lime-200 shadow-md border-2 border-black rounded-lg px-3 py-2 mb-4">
               <div className="block text-gray-700 text-2xl font-semibold py-2 px-2">
                 {listName}
